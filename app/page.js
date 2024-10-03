7f1cc9169c315472dd55daa8faf5ed5da13c1f80
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2024-10-03 23:00:57 +0600

    Hero , About , Explore Wolrd, features section completed

--- app/page.js
+++ app/page.js
@@ -1,18 +1,36 @@
 import { Footer, Navbar } from "@/components";
-import { About, Explore, Feedback, GetStarted, Hero, Insight, WhatsNew, World } from "@/sections";
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insight,
+  WhatsNew,
+  World,
+} from "@/sections";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-primary-black overflow-hidden">
       <Navbar />
       <Hero />
-      <About />
-      <Explore />
-      <GetStarted/>
-      <WhatsNew/>
-      <World/>
-      <Insight/>
-      <Feedback/>
+      <div className="relative">
+        <About />
+        <div className="gradient-03 z-0" />
+        <Explore />
+      </div>
+      <div className="relative">
+        <GetStarted />
+        <div className="gradient-04 z-0" />
+        <WhatsNew />
+      </div>
+      <World />
+      <div className="relative">
+        <Insight />
+        <div className="gradient-04 z-0" />
+        <Feedback />
+      </div>
       <Footer />
     </div>
   );
--- components/StarSteps.jsx
+++ components/StarSteps.jsx
@@ -1,8 +1,13 @@
+import styles from "@/styles"
 
-
-function StarSteps() {
+function StarSteps({text, number }) {
   return (
-    <div>StarSteps</div>
+    <div className={`${styles.flexCenter} flex-row `}>
+      <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d] `}>
+        <p className="text-white font-bold text-[20px]">0{number}</p>
+      </div>
+      <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32px]">{text}</p>
+    </div>
   )
 }
 
