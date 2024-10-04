fde2517f2a744f97123a190006546090c3f3d52f
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2024-10-04 23:51:08 +0600

    maps responsive done

--- sections/World.jsx
+++ sections/World.jsx
@@ -35,8 +35,14 @@ function World() {
             alt="map"
             className="w-full h-full object-cover"
           />
-          <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-          <img src="/people-01.png" alt="people1" className="w-full h-full"/>
+          <div className="absolute bottom-14 right-5 md:bottom-20 md:right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+            <img src="/people-01.png" alt="people1" className="w-full h-full" />
+          </div>
+          <div className="absolute top-5 left-5 md:top-10 md:left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+            <img src="/people-02.png" alt="people2" className="w-full h-full" />
+          </div>
+          <div className="absolute left-[40%] top-1/2 -translate-y-1/2 md:left-[44%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+            <img src="/people-03.png" alt="people3" className="w-full h-full" />
           </div>
         </motion.div>
       </motion.div>
